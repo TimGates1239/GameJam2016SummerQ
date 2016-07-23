5476bd78d97cffacaaeb3d1907575aeac7d58039
Tim Gates <sly12391237@gmail.com>
2016-07-23 11:43:03 -0600

    New project push attempt

---
+++ GameJamGame/Intermediate/Build/Win64/UE4Editor/Development/GameJamGame/GameJamGame.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_GAMEJAMGAME_H__
+#define __AUTO_GAMEJAMGAME_H__
+//Last Write: 7/23/2016 12:26:06 AM +00:00
+#include "C:\Users\Tim Gates\Documents\Unreal Projects\GameJam\GameJam2016SummerQ\GameJamGame\Source\GameJamGame\GameJamGame.h"
+#endif//__AUTO_GAMEJAMGAME_H__
