e15e35468ee46df7ff322329f95e0bff2032e3b8
Tim Gates <sly12391237@gmail.com>
2016-07-23 11:39:45 -0600

    Redoing

--- GameJamGame/Intermediate/Build/Win64/UE4Editor/Development/GameJamGame/GameJamGame.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_GAMEJAMGAME_H__
-#define __AUTO_GAMEJAMGAME_H__
-//Last Write: 7/23/2016 12:26:06 AM +00:00
-#include "C:\Users\Tim Gates\Documents\Unreal Projects\GameJam\GameJam2016SummerQ\GameJamGame\Source\GameJamGame\GameJamGame.h"
-#endif//__AUTO_GAMEJAMGAME_H__
--- GameJamGame/Intermediate/Build/Win64/UE4Editor/Development/GameJamGame/PCH.GameJamGame.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:\Users\Tim Gates\Documents\Unreal Projects\GameJam\GameJam2016SummerQ\GameJamGame\Source\GameJamGame\GameJamGame.h"
